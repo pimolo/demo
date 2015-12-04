a61f4510dd451659ba48d556f3ae11fe8bb8c54e
Rémi Andrieux <pimolo10300@gmail.com>
2015-12-04 10:20:49 +0100

    First commit

---
+++ config.rb
@@ -0,0 +1,3 @@
+http_path = 'public/'
+css_dir = "compiled/css"
+sass_dir = "sass"
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require('gulp');
+var compass = require('gulp-compass');
+var browserSync = require('browser-sync').create();
+var minifyCss = require('gulp-minify-css');
+var jade = require('gulp-jade');
+
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./public"
+    });
+
+    gulp.src('fonts-fontface/*').pipe(gulp.dest('public/fonts'));
+    gulp.src('img/*').pipe(gulp.dest('public/images'));
+    gulp.watch("sass/**/*.scss", ['compass']);
+    gulp.watch("templates/*.jade", ['templates']);
+});
+gulp.task('compass', function() {
+    return gulp.src('sass/*.scss')
+        .pipe(compass({
+            config_file: 'config.rb',
+            css: 'compiled/css',
+            sass: 'sass/'
+        }))
+        .on('error', function(error) {
+            console.log(error.message);
+            this.emit('end');
+        })
+        .pipe(gulp.dest('compiled/css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('public'))
+        .pipe(browserSync.stream())
+    ;
+});
+
+gulp.task('templates', function() {
+    return gulp.src('templates/index.jade')
+        .pipe(jade())
+        .pipe(gulp.dest('public/'))
+        .pipe(browserSync.reload({stream: true}))
+    ;
+});
+
+gulp.task('default', ['templates', 'compass', 'serve']);
